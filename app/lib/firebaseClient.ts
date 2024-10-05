30b0cfed959be4d3e4ea7747b209c4ce8d8d3a44
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-10-04 23:39:39 -0300

    fix: elimine claves secretas que podian poner en peligro cuentas

--- app/lib/firebaseClient.ts
+++ app/lib/firebaseClient.ts
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getStorage} from  "firebase/storage";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIK2zjhpD0Blc0IalRgNqC71634f4KiUI",
-  authDomain: "angiediaz-df855.firebaseapp.com",
-  projectId: "angiediaz-df855",
-  storageBucket: "angiediaz-df855.appspot.com",
-  messagingSenderId: "752638920803",
-  appId: "1:752638920803:web:04d29a1af8d7e1ada05056",
-  measurementId: "G-6K0GBRS0PV"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MESUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
-export {storage};
+export { storage };
 export default app;
