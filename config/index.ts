818bfdd7c4c272854c62a7ed4b7593ca65a02290
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-11-13 15:30:14 -0300

    feature: nueva configuracion por defecto para toda la app

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,44 @@
 import { z, ZodError } from "zod";
 
+/* Muy importante: Recordar que process.env no esta disponible en el cliente.
+ * Por ende, cuando intente solicitar cualquier recurso, y este falle,
+ * se vera un mensaje que advierta de esto para descartar cualquier advertencia
+ * En cualquier caso, se retornara una valor por defecto que puede o no
+ * contener informacion importante para el cliente, ejemplo: API_URL
+ * */
+
+const obtenerConfiguracionATravesDeLosParametros =
+  (): ParametrosDeConfiguracionDeLaApp => {
+    try {
+      return EsquemaParaLosParametrosDeConfiguracionDeLaApp.parse(process.env);
+    } catch (error) {
+      console.log(
+        "Advertencia: process.env no esta disponible en el cliente. Se retornara un valor por defecto.",
+      );
+      const advertencias = (error as ZodError).issues.map((issue) => ({
+        advertencia: issue.message,
+        campo: issue.path.toString(),
+      }));
+      console.table(advertencias);
+      return {
+        API_URL: "http://10.9.121.238:3001/api",
+        BASE_URL: "http://10.9.121.238:3001",
+        MQTT_URL: "mqtt://localhost:3000",
+        DATABASE_URL: "file:test.db",
+        JWT_SECRET: "jwt_secret",
+        NODE_ENV: "DEV",
+        CI: "off",
+        FIREBASE_API_KEY: "firebase_api_key",
+        FIREBASE_AUTH_DOMAIN: "firebase_auth_domain",
+        FIREBASE_PROJECT_ID: "firebase_proyect_id",
+        FIREBASE_STORAGE_BUCKET: "firebase_storage_bucket",
+        FIREBASE_MESSAGING_SENDER_ID: "firebase_messaging_sender_id",
+        FIREBASE_APP_ID: "firebase_app_id",
+        FIREBASE_MEASUREMENT_ID: "firebase_measurement_id",
+      };
+    }
+  };
+
 const EsquemaParaLosParametrosDeConfiguracionDeLaApp = z.object({
   API_URL: z.string({ message: "API_URL es requerido" }),
   BASE_URL: z.string({ message: "BASE_URL es requerido" }),
@@ -25,32 +64,8 @@ const EsquemaParaLosParametrosDeConfiguracionDeLaApp = z.object({
   FIREBASE_MEASUREMENT_ID: z.string(),
 });
 
-const obtenerConfiguracionATravesDeLosParametros = () => {
-  try {
-    return EsquemaParaLosParametrosDeConfiguracionDeLaApp.parse(process.env);
-  } catch (error) {
-    const mensaje = (error as ZodError).issues.at(0)?.message;
-    console.error(process.env)
-    console.error(mensaje);
-    return {
-      API_URL: "http://10.9.121.238:3001/api",
-      BASE_URL: "http://10.9.121.238:3001",
-      MQTT_URL: "mqtt://10.9.121.238",
-      DATABASE_URL: "file:test.db",
-      JWT_SECRET: "jwt_secret",
-      NODE_ENV: "DEV",
-      CI: "off",
-      FIREBASE_API_KEY: "firebase_api_key",
-      FIREBASE_AUTH_DOMAIN: "firebase_auth_domain",
-      FIREBASE_PROJECT_ID: "firebase_proyect_id",
-      FIREBASE_STORAGE_BUCKET: "firebase_storage_bucket",
-      FIREBASE_MESSAGING_SENDER_ID: "firebase_messaging_sender_id",
-      FIREBASE_APP_ID: "firebase_app_id",
-      FIREBASE_MEASUREMENT_ID: "firebase_measurement_id",
-    }
-  }
-};
+type ParametrosDeConfiguracionDeLaApp = z.infer<
+  typeof EsquemaParaLosParametrosDeConfiguracionDeLaApp
+>;
 
-type EsquemaParaLosParametrosDeConfiguracionDeLaApp = z.infer<typeof EsquemaParaLosParametrosDeConfiguracionDeLaApp>
-
-export const config: EsquemaParaLosParametrosDeConfiguracionDeLaApp = obtenerConfiguracionATravesDeLosParametros();
+export const config = obtenerConfiguracionATravesDeLosParametros();
