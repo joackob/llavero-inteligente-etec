c858f32d6fe2d5b329c6597159e4325b307fc603
diaz-11 <angiediazvilca12@gmail.com>
2024-11-19 19:46:39 -0300

    agregue array con links de imagenes

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,12 @@ export default function Page() {
   // if (error) {
   //   return <MensajeDeAlerta mensaje={error} />; // Muestra mensaje de error si hubo un problema
   // }
+  //imagenes 
+  const Urls = ['https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165010.jpg?alt=media&token=1549dbf4-e5d4-4c24-97b2-9e58c0e97817', 'https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454', 'https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165425.jpg?alt=media&token=a63fdc49-070d-4591-aebe-89adbdef219c',
+'https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165556.jpg?alt=media&token=1b6a3f10-c515-428f-aa58-96372f00916c','https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165636.jpg?alt=media&token=a611e1f8-5580-426d-9233-b53357371ce0',
+'https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165710.jpg?alt=media&token=9516ab70-8b15-44c0-a861-dd7195d4024a',
+'https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165854.jpg?alt=media&token=6fad63ee-8bad-4e10-a934-2bfbbbd837a6']
+
 
   return (
     <>
