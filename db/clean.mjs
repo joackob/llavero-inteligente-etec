bacae02b633d4c96f6e874e90b5186d460898369
diaz-11 <angiediazvilca12@gmail.com>
2024-11-20 19:16:18 -0300

    Agregue la url de la imagen a los espacios y el clean:db para borrar los espacios de la base de datos

--- db/clean.mjs
+++ db/clean.mjs
@@ -1,10 +1,18 @@
-import { PriismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 (async () => {
-  const db = new PriismaClient();
+  const db = new PrismaClient();
   await db.$connect();
   try {
     await db.llaves.deleteMany();
+    console.log("\nBase de datos borrada\n");
+  } catch (error) {
+    if (error instanceof PrismaClient.PrismaClientKnownRequestError) {
+      console.error(`\n${error.message}\n`);
+    }
+    if (error instanceof PrismaClient.PrismaClientUnknownRequestError) {
+      console.error("\nhubo un error al inicializar la base de datos\n");
+    }
   } finally {
     await db.$disconnect();
   }
--- db/init.mjs
+++ db/init.mjs
@@ -8,31 +8,68 @@ import { PrismaClient } from "@prisma/client";
     try {
       await db.llaves.createMany({
         data: [
-          { espacio: "aula 213" },
-          { espacio: "aula 214" },
-          { espacio: "aula 314" },
-          { espacio: "aula 313" },
-          { espacio: "aula 205" },
-          { espacio: "aula 204" },
-          { espacio: "aula 104" },
-          { espacio: "aula 105" },
-          { espacio: "aula 301" },
-          { espacio: "aula 302" },
-          { espacio: "aula 303" },
+          {
+            espacio: "aula 213",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 214",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 314",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 313",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 205",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 204",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 104",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 105",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 301",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 302",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
+          {
+            espacio: "aula 303",
+            urls: "https://firebasestorage.googleapis.com/v0/b/angiediaz-df855.appspot.com/o/llavero-inteligente%2F20241004_165405.jpg?alt=media&token=ae1aa988-51b5-4b70-8fae-421652a45454",
+          },
         ],
       });
       console.log("\nBase de datos inicializada\n");
     } catch (error) {
+      console.error("\nError al conectar con la base de datos\n");
+      console.error(error);
+      /*
       if (error instanceof PrismaClient.PrismaClientKnownRequestError) {
         console.error(`\n${error.message}\n`);
       }
       if (error instanceof PrismaClient.PrismaClientUnknownRequestError) {
         console.error("\nhubo un error al inicializar la base de datos\n");
-      }
+      }*/
     } finally {
       await db.$disconnect();
     }
   } catch (error) {
     console.error("\nError al conectar con la base de datos\n");
+    console.error(error);
   }
 })();
